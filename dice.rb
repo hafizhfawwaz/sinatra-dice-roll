cfb05ff46d57e004b2d7a12e030b8e202e2d3de4
Fawwaz Hafizh <100624670+hafizhfawwaz@users.noreply.github.com>
2025-01-26 21:35:04 +0000

    second commit

--- dice.rb
+++ dice.rb
@@ -2,3 +2,12 @@
 # bundle install
 
 require "sinatra"
+require "sinatra/reloader"
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
