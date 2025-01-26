955c0567e4587eb9a55053e5fc34b8a80fa50947
Fawwaz Hafizh <100624670+hafizhfawwaz@users.noreply.github.com>
2025-01-26 22:40:19 +0000

    sixth commit

--- dice.rb
+++ dice.rb
@@ -40,7 +40,7 @@ get("/dice/2/10") do
 end    
   
 get ("/dice/1/20") do
-  first_die = rand(1..20)
+  @first_die = rand(1..20)
   @outcome = "You rolled a #{first_die}."
   erb(:one_twenty)
 end
@@ -55,3 +55,13 @@ get("/dice/5/4") do
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
   erb(:five_four)
 end    
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+  erb(:one_hundred_six)
+end
